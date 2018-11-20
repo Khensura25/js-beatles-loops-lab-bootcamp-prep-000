cf748d7584ac8ca150e79052136ad27ba5aec9c0
khensura.loveel Love EL <khensura.loveel@gmail.com>
2018-11-20 01:59:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // add solution here
+function theBeatlesPlay(array, intstruments) {
+  let newArray = [];
+  
+  for (let i = 0; i < array.length; i++) {
+      
+      newArray.push(`${array[i]} plays ${intstruments[i]}`)
+  }
+   return newArray;
+}
+
+
+function johnLennonFacts(array) {
+  let i = 0;
+ while( i < array.length) {
+   array[i] = array[i] + "!!!";
+ }
+ return array;
+}
